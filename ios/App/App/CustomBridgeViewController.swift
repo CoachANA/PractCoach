afe51232fbef5fe79170908a42efc8d019661e2e
Azza NAFTI <azza.nafti87@gmail.com>
2026-08-12 16:24:40 +0200

    Fix iOS microphone permission in Capacitor WebView

---
+++ ios/App/App/CustomBridgeViewController.swift
@@ -0,0 +1,27 @@
+import UIKit
+import WebKit
+import Capacitor
+
+class CustomBridgeViewController: CAPBridgeViewController, WKUIDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        webView?.uiDelegate = self
+    }
+
+    @available(iOS 15.0, *)
+    func webView(
+        _ webView: WKWebView,
+        requestMediaCapturePermissionFor origin: WKSecurityOrigin,
+        initiatedByFrame frame: WKFrameInfo,
+        type: WKMediaCaptureType,
+        decisionHandler: @escaping (WKPermissionDecision) -> Void
+    ) {
+        if origin.host == "www.practcoach.com" {
+            decisionHandler(.grant)
+        } else {
+            decisionHandler(.prompt)
+        }
+    }
+}
