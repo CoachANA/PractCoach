08ed1154f3812533b83cc6fc20cd13089e7a6ee6
Azza NAFTI <azza.nafti87@gmail.com>
2026-08-13 00:06:18 +0200

    Add iOS microphone diagnostic and increment build to 24

--- ios/App/App/CustomBridgeViewController.swift
+++ ios/App/App/CustomBridgeViewController.swift
@@ -7,6 +7,16 @@ class CustomBridgeViewController: CAPBridgeViewController, WKUIDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let alert = UIAlertController(
+            title: "DIAG iOS",
+            message: "CustomBridgeViewController actif",
+            preferredStyle: .alert
+        )
+
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+
+        present(alert, animated: true)
+
         webView?.uiDelegate = self
     }
 
